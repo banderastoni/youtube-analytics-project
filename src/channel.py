0c72a4a818bbfb70a5deb03b3e56183e854a0124
banderastoni <banderastoni@rambler.ru>
2023-07-06 06:31:39 +0300

    Homework 2 done: modify attributes of Chanel add class method get_service() add method to_json() created .json file with Channel data

--- src/channel.py
+++ src/channel.py
@@ -3,11 +3,6 @@ import os
 from googleapiclient.discovery import build
 
 
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
-
 class Channel:
     """Класс для ютуб-канала"""
     # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
@@ -18,9 +13,31 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        self.title = channel['items'][0]['snippet']['title']
+        self.url = 'https://www.youtube.com/channel/' + channel_id
+        self.description = channel['items'][0]['snippet']['description']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        printj(channel)
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        """Геттер channel_id"""
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод, возвращающий объект для работы с YouTube API"""
+        return cls.youtube
+
+    def to_json(self, file):
+        """Метод, сохраняющий в файл значения атрибутов экземпляра класса"""
+        with open(file, 'w') as fp:
+            json.dump(self.__dict__, fp, indent=2, ensure_ascii=False)
