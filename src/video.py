890b8748ae0d3e275b8f268ee2f004812bb285a6
banderastoni <banderastoni@rambler.ru>
2023-07-23 23:15:24 +0300

    Homework 6 done: add try/except for class Video

--- src/video.py
+++ src/video.py
@@ -8,21 +8,21 @@ class Video:
         # билд для работы с API Youtube
         youtube = Channel.get_service()
 
-        # try:
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=video_id
-                                               ).execute()
-        self.video_id = video_id
-        self.title = video_response['items'][0]['snippet']['title']
-        self.url = "https://www.youtube.com/watch?v=" + video_id
-        self.video_view_count = video_response['items'][0]['statistics']['viewCount']
-        self.video_like_count = video_response['items'][0]['statistics']['likeCount']
-        # except IndexError:
-        #     self.video_id = video_id
-        #     self.title = None
-        #     self.url = None
-        #     self.video_view_count = None
-        #     self.video_like_count = None
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.video_id = video_id
+            self.title = video_response['items'][0]['snippet']['title']
+            self.url = "https://www.youtube.com/watch?v=" + video_id
+            self.video_view_count = video_response['items'][0]['statistics']['viewCount']
+            self.video_like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.video_view_count = None
+            self.video_like_count = None
 
     def __str__(self):
         return self.title
